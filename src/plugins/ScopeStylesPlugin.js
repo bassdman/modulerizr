a47b3cd1943ba2fbc65aa5c2ed9d2ed576318bb9
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-12-08 20:29:13 +0100

    finished scoped styles plugin

--- src/plugins/ScopeStylesPlugin.js
+++ src/plugins/ScopeStylesPlugin.js
@@ -1,11 +1,26 @@
 const cheerio = require('cheerio');
+const csstree = require('css-tree');
+
 
 async function ScopeStylesPlugin(modulerizr, currentFile) {
     const $ = cheerio.load(currentFile.content);
     $('*').attr('data-v-' + currentFile.id, "")
     modulerizr.log('start ScopeStylesPlugin ' + currentFile.id);
 
-    const styleTags = $('style');
+    const $styleTags = $('style[scoped]');
+    $styleTags.each((i, e) => {
+        const $currentStyles = $(e);
+        const ast = csstree.parse($currentStyles.html());
+        csstree.walk(ast, function(node) {
+            if (node.type === 'ClassSelector') {
+                node.name = `${node.name}[data-v-${currentFile.id}]`;
+            }
+        });
+        const parsedStyles = csstree.generate(ast);
+        $currentStyles.html(parsedStyles);
+        $currentStyles.removeAttr("scoped")
+    });
+
     return {
         content: $.html($(':root'))
     }
