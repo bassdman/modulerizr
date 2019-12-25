45c4b2d69af1a39f88c98d1e294d62c5ddd61f7d
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-12-25 08:06:00 +0100

    updated debug plugin

--- src/plugins/DebugPlugin.js
+++ src/plugins/DebugPlugin.js
@@ -7,6 +7,12 @@ class DebugPlugin {
         this.config = pluginconfig;
     }
     async apply(modulerizr) {
+        if (this.config.apply)
+            this.config.apply(modulerizr);
+
+        if (this.config.createDebugFile === false)
+            return;
+
         modulerizr.plugins.on('deploy', async() => {
             return await fs.writeFile(path.join(modulerizr.config.dest, 'modulerizr-debug.config.json'), JSON.stringify({ config: modulerizr.config, store: modulerizr.store.queryOne('$') }, null, 1));
         });
