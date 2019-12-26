26536560a3141f742a6015271a0b0287a0773868
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-12-26 16:55:24 +0100

    refactoring

--- src/plugins/PrerenderScriptPlugin.js
+++ src/plugins/PrerenderScriptPlugin.js
@@ -13,30 +13,24 @@ class PrerenderScriptPlugin {
     apply(modulerizr) {
         modulerizr.plugins.on('afterRender', async() => {
 
-            return await modulerizr.store.$each('$.src.*', async($, currentFile, currentPath) => {
-                const $scriptTags = $(`script[${this.serversideAttributeName}]`);
+            return await modulerizr.store.$each(`$.src.*/script[${this.serversideAttributeName}]`, async($currentScript, currentFile, currentPath) => {
+                const tempFileHash = crypto.createHash('md5').update($currentScript.html().trim()).digest("hex").substring(0, 8);
+                const tempFilename = "./_temp/temp_" + tempFileHash + '.js';
 
-                await foreachPromise($scriptTags, async e => {
-                    const $currentScript = $(e);
+                await fs.ensureDir(path.dirname(tempFilename));
+                await fs.writeFile(tempFilename, $currentScript.html().trim());
 
-                    const tempFileHash = crypto.createHash('md5').update($currentScript.html().trim()).digest("hex").substring(0, 8);
-                    const tempFilename = "./_temp/temp_" + tempFileHash + '.js';
+                const returnValue = require(path.join(modulerizr.config._rootPath, tempFilename));
 
-                    await fs.ensureDir(path.dirname(tempFilename));
-                    await fs.writeFile(tempFilename, $currentScript.html().trim());
+                const data = typeof returnValue.data == 'function' ? returnValue.data() : returnValue.data;
 
-                    const returnValue = require(path.join(modulerizr.config._rootPath, tempFilename));
+                const embeddedComponentId = $currentScript.parent('[data-component-instance]').attr('data-component-instance');
+                const embeddedComponent = modulerizr.store.queryOne(`$.embeddedComponents.id_${embeddedComponentId}`);
+                const componentPrerenderData = modulerizr.store.queryOne(`$.component.id_${embeddedComponent.componentId}.preRenderData`) || {};
 
-                    const data = typeof returnValue.data == 'function' ? returnValue.data() : returnValue.data;
-
-                    const embeddedComponentId = $currentScript.parent('[data-component-instance]').attr('data-component-instance');
-                    const embeddedComponent = modulerizr.store.queryOne(`$.embeddedComponents.id_${embeddedComponentId}`);
-                    const componentPrerenderData = modulerizr.store.queryOne(`$.component.id_${embeddedComponent.componentId}.preRenderData`) || {};
-
-                    const mappedData = Object.assign({}, componentPrerenderData, data);
-                    modulerizr.store.value(`$.component.id_${embeddedComponent.componentId}.preRenderData`, mappedData);
-                });
-            })
+                const mappedData = Object.assign({}, componentPrerenderData, data);
+                modulerizr.store.value(`$.component.id_${embeddedComponent.componentId}.preRenderData`, mappedData);
+            });
         })
 
         modulerizr.plugins.on('finish', async() => {
